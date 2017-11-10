1993e018dce7270242387270b7250f8ecd1ebf60
sngo1 <sngo1@stuy.edu>
2017-11-10 00:24:50 -0500

    Changes

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
-from utils import auth, edit, database
+# Samantha Ngo
+# Softdev -- pd7
+# hw13
+# 2017-11-09
 
+from flask import Flask, render_template
+import urllib2, json
 
 app = Flask(__name__)
-app.secret_key = os.urandom(32)
+content = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=hnMJycsr0ttvB0vkB7NqgGwp1X5z2bHmviBia3DC")
+landslides = urllib2.urlopen("https://data.nasa.gov/resource/tfkf-kniw.json")
 
-
-# Login Authentication
+# test = content.info()
+info = content.read()
+data = json.loads(info)
+    
 @app.route('/')
-def hello():
-    return render_template('launch.html', test = "Hello World")
+def launch():
+    image1 = data["hdurl"]
+    explanation = data["explanation"]
+    
+    return render_template('launch.html', image1 = image1, explanation = explanation)
 
 if __name__ == "__main__":
     app.debug = True
